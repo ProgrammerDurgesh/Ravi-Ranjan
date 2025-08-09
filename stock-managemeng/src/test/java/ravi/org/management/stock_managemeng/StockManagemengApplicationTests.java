b0d64c563fe4112b520e641f6845d692d75af7de
Durgesh Yadav <durgeshyadavgpm16@gmail.com>
2025-08-09 16:31:25 +0530

    first commit

---
+++ stock-managemeng/src/test/java/ravi/org/management/stock_managemeng/StockManagemengApplicationTests.java
@@ -0,0 +1,13 @@
+package ravi.org.management.stock_managemeng;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockManagemengApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
